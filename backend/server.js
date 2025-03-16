50367e5e886e18832643f9fd19837b75359c5cb1
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-16 12:57:39 +0000

    Enhance error handling in server.js for quote submission and improve response messages

--- backend/server.js
+++ backend/server.js
@@ -28,13 +28,11 @@ function randomQuote() {
   return quotes[index];
 }
 
-
 app.get("/api/quote", (_, res) => {
   const quote = randomQuote();
   console.log(quote);
   res.json(quote);
 });
-
 
 app.post("/", (req, res) => {
   const bodyBytes = [];
@@ -46,25 +44,49 @@ app.post("/", (req, res) => {
       body = JSON.parse(bodyString);
     } catch (error) {
       console.error(`Failed to parse body ${bodyString} as JSON: ${error}`);
-      res.status(400).send("Expected body to be JSON.");
+      res.status(400).json({ succes: false, msg: "Expected body to be JSON." });
       return;
     }
+
     if (typeof body != "object" || !("quote" in body) || !("author" in body)) {
       console.error(
         `Failed to extract quote and author from post body: ${bodyString}`
       );
-      res
-        .status(400)
-        .send(
-          "Expected body to be a JSON object containing keys quote and author."
-        );
+      res.status(400).json({
+        succes: false,
+        msg: "Expected body to be a JSON object containing keys quote and author.",
+      });
       return;
     }
+
+    if (!body.quote && !body.author) {
+      console.error(`Both quote and author must be provided`);
+      res.status(400).json({
+        success: false,
+        msg: "Both quote and author must be provided`",
+      });
+      return;
+    }
+
+    if (!body.quote) {
+      console.error(`Quote must be provided`);
+      res.status(400).json({ success: false, msg: "Please provide a quote" });
+      return;
+    }
+
+    if (!body.author) {
+      console.error(`Author must be provided`);
+      res
+        .status(400)
+        .json({ success: false, msg: "Please provide a an author" });
+      return;
+    }
+
     quotes.push({
       quote: body.quote,
       author: body.author,
     });
-    res.json(body);
+    res.status(201).json({ succes: true, quote: body });
   });
 });
 
