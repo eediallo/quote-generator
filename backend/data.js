3694ae7f4b14d526bc1f515922339a194e2b875c
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-16 17:50:13 +0000

    Add initial quotes data to backend

---
+++ backend/data.js
@@ -0,0 +1,11 @@
+export const quotes = [
+  {
+    quote:
+      "Either write something worth reading or do something worth writing.",
+    author: "Benjamin Franklin",
+  },
+  {
+    quote: "I should have been more kind.",
+    author: "Clive James",
+  },
+];
