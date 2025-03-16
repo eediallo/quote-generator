56568cbc4f77d39e0fa3491561525e9ef3c0d224
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-16 12:03:52 +0000

    Refactor app.js to use Axios for API requests and update index.html to load Axios script

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -1,63 +1,53 @@
+const generateQuoteBtn = document.querySelector("#generate-quote-btn");
+const quoteEl = document.querySelector("#displayed-quote");
+const quoteInput = document.querySelector("#quote");
+const authorInput = document.querySelector("#author");
+const addQuoteBtn = document.querySelector("#add-quote");
 
+const baseUrl =
+  "https://elhadj-abdoul-diallo-quote-generator-still-darkness-1788.fly.dev/";
+const localhost = "http://localhost:3000/";
 
-const generateQuoteBtn = document.querySelector('#generate-quote-btn')
-const quoteEl = document.querySelector('#displayed-quote')
-const quoteInput = document.querySelector('#quote')
-const authorInput = document.querySelector('#author')
-const addQuoteBtn = document.querySelector('#add-quote')
-
-
-const baseUrl = 'https://elhadj-abdoul-diallo-quote-generator-still-darkness-1788.fly.dev/'
-const localhost = 'http://localhost:3000/'
-
-async function fetchQuote(){
-    try{
-        const resp = await fetch(`${localhost}api/quote`, {
-            method: 'GET',
-            headers: {'content-type' : 'application/json'}
-        })
-        if (!resp.ok) {
-            throw new Error(`Failed to fetch quote: ${resp.status}`)
-        }
-        return await resp.json()
-    }catch(err){
-        console.error(err.message)
-    }
+// get quote
+async function fetchQuote() {
+  try {
+    const resp = await axios.get(`${localhost}api/quote`);
+    return resp.data;
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
+// post quote
 async function addQuote() {
-    const quote = {
+  try {
+    const { data } = await axios.post(
+      localhost,
+      {
         quote: quoteInput.value.trim(),
-        author: authorInput.value.trim()
-    };
-
-    try {
-        const resp = await fetch(localhost, {
-            method: 'POST',
-            headers: { 'content-type': 'application/json' },
-            body: JSON.stringify(quote)
-        });
-
-        if (!resp.ok) {
-            throw new Error(`Failed to add quote: ${resp.status}`);
-        }
-        // clear input field after submission
-        quoteInput.value = ''
-        authorInput.value = ''
-    } catch (err) {
-        console.error(err.message);
-    }
+        author: authorInput.value.trim(),
+      },
+      {
+        headers: { "content-type": "application/json" },
+      }
+    );
+    quoteInput.value = "";
+    authorInput.value = "";
+    return data
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
 //event to get and display quote
-generateQuoteBtn.addEventListener('click', async (e)=>{
-    e.preventDefault()
-    const quote = await fetchQuote()
-    quoteEl.textContent = `${quote.quote} - ${quote.author}`
-})
+generateQuoteBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const quote = await fetchQuote();
+  quoteEl.textContent = `${quote.quote} - ${quote.author}`;
+});
 
 // event to post quote
-addQuoteBtn.addEventListener('click', async(e)=>{
-    e.preventDefault()
-    await addQuote()
-})
+addQuoteBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  await addQuote();
+});
