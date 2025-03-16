251434822fc793bf674330b12ed9e7ce749cbd76
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-16 13:01:44 +0000

    Fix formatting of error message element selection in app.js

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -3,7 +3,7 @@ const quoteEl = document.querySelector("#displayed-quote");
 const quoteInput = document.querySelector("#quote");
 const authorInput = document.querySelector("#author");
 const addQuoteBtn = document.querySelector("#add-quote");
-const errorMsgEl = document.querySelector('#errMsg')
+const errorMsgEl = document.querySelector("#errMsg");
 
 const baseUrl =
   "https://elhadj-abdoul-diallo-quote-generator-still-darkness-1788.fly.dev/";
@@ -21,24 +21,24 @@ async function fetchQuote() {
 
 // post quote
 async function addQuote() {
-    try {
-        const resp = await axios.post(
-            localhost,
-            {
-                quote: quoteInput.value.trim(),
-                author: authorInput.value.trim(),
-            },
-            {
-                headers: { "content-type": "application/json" },
-            }
-        );
-        quoteInput.value = "";
-        authorInput.value = "";
-        errorMsgEl.textContent = "Quote added successfully!";
-        console.log(resp);
-    } catch (err) {
-        errorMsgEl.textContent = err.response.data.msg;
-    }
+  try {
+    const resp = await axios.post(
+      localhost,
+      {
+        quote: quoteInput.value.trim(),
+        author: authorInput.value.trim(),
+      },
+      {
+        headers: { "content-type": "application/json" },
+      }
+    );
+    quoteInput.value = "";
+    authorInput.value = "";
+    errorMsgEl.textContent = "Quote added successfully!";
+    console.log(resp);
+  } catch (err) {
+    errorMsgEl.textContent = err.response.data.msg;
+  }
 }
 
 //event to get and display quote
