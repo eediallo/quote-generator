ca8593f33be923216be521ff72f20a82f22dc4aa
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-16 17:50:26 +0000

    Refactor server.js to use quoteRouter for quote handling and remove inline quote management

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { quoteRouter } from "./routes/quote.js";
 
 const staticDir = new URL("../frontend/static", import.meta.url).pathname;
 
@@ -11,83 +12,8 @@ app.use(cors());
 //setup static  files and middleware
 app.use(express.static(staticDir));
 
-const quotes = [
-  {
-    quote:
-      "Either write something worth reading or do something worth writing.",
-    author: "Benjamin Franklin",
-  },
-  {
-    quote: "I should have been more kind.",
-    author: "Clive James",
-  },
-];
-
-function randomQuote() {
-  const index = Math.floor(Math.random() * quotes.length);
-  return quotes[index];
-}
-
-app.get("/api/quote", (_, res) => {
-  const quote = randomQuote();
-  res.json(quote);
-});
-
-app.post("/", (req, res) => {
-  const bodyBytes = [];
-  req.on("data", (chunk) => bodyBytes.push(...chunk));
-  req.on("end", () => {
-    const bodyString = String.fromCharCode(...bodyBytes);
-    let body;
-    try {
-      body = JSON.parse(bodyString);
-    } catch (error) {
-      console.error(`Failed to parse body ${bodyString} as JSON: ${error}`);
-      res.status(400).json({ succes: false, msg: "Expected body to be JSON." });
-      return;
-    }
-
-    if (typeof body != "object" || !("quote" in body) || !("author" in body)) {
-      console.error(
-        `Failed to extract quote and author from post body: ${bodyString}`
-      );
-      res.status(400).json({
-        succes: false,
-        msg: "Expected body to be a JSON object containing keys quote and author.",
-      });
-      return;
-    }
-
-    if (!body.quote && !body.author) {
-      console.error(`Both quote and author must be provided`);
-      res.status(400).json({
-        success: false,
-        msg: "Both quote and author must be provided",
-      });
-      return;
-    }
-
-    if (!body.quote) {
-      console.error(`Quote must be provided`);
-      res.status(400).json({ success: false, msg: "Please provide a quote" });
-      return;
-    }
-
-    if (!body.author) {
-      console.error(`Author must be provided`);
-      res
-        .status(400)
-        .json({ success: false, msg: "Please provide a an author" });
-      return;
-    }
-
-    quotes.push({
-      quote: body.quote,
-      author: body.author,
-    });
-    res.status(201).json({ succes: true, quote: body });
-  });
-});
+// quote router
+app.use("/", quoteRouter);
 
 app.listen(port, () => {
   console.error(`Quote server listening on port ${port}`);
