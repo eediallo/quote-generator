5718512bfb08d5a1b645a015119754fddf3167f9
eediallo <elhhabdouldiallo@gmail.com>
2025-03-17 12:05:59 +0000

    Add server setup with Express and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import { quoteRouter } from "./routes/quote.js";
+import { connectDB } from "./db/connect.js";
+import dotenv from 'dotenv'
+dotenv.config()
+
+const staticDir = new URL("../frontend/static", import.meta.url).pathname;
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+//setup static  files and middleware
+app.use(express.static(staticDir));
+
+// quote router
+app.use("/", quoteRouter);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.error(`Quote server listening on port ${port}`);
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+start()
