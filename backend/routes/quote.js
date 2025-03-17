b17a921c0d10960dc19338f2e96162e161df1343
eediallo <elhhabdouldiallo@gmail.com>
2025-03-17 13:49:25 +0000

    adds endpoint to get all quotes

--- backend/routes/quote.js
+++ backend/routes/quote.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createQuote, getQuote } from "../controllers/quotes.js";
+import { createQuote, getAllQuotes, getQuote } from "../controllers/quotes.js";
 
 const quoteRouter = express.Router();
 
-quoteRouter.get("/api/v1/quotes", getQuote);
-
+quoteRouter.get("/api/v1/quote", getQuote);
+quoteRouter.get("/api/v1/quotes", getAllQuotes);
 quoteRouter.post("/", createQuote);
 
 export { quoteRouter };
