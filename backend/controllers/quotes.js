68d48b3e2068c8c8687646a171c76c12e084ede3
eediallo <elhhabdouldiallo@gmail.com>
2025-03-17 12:46:47 +0000

    Refactor createQuote function to use async/await and improve error handling

--- backend/controllers/quotes.js
+++ backend/controllers/quotes.js
@@ -1,4 +1,5 @@
 import { quotes } from "../data.js";
+import { Quote } from "../models/quotes.js";
 
 function randomQuote() {
   const index = Math.floor(Math.random() * quotes.length);
@@ -12,68 +13,52 @@ const getQuote = () => {
   };
 };
 
-const createQuote = () => {
-  (req, res) => {
-    const bodyBytes = [];
-    req.on("data", (chunk) => bodyBytes.push(...chunk));
-    req.on("end", () => {
-      const bodyString = String.fromCharCode(...bodyBytes);
-      let body;
-      try {
-        body = JSON.parse(bodyString);
-      } catch (error) {
-        console.error(`Failed to parse body ${bodyString} as JSON: ${error}`);
-        res
-          .status(400)
-          .json({ succes: false, msg: "Expected body to be JSON." });
-        return;
-      }
+const createQuote = async (req, res) => {
+  let quote;
+  try {
+    quote = await Quote.create(req.body);
+  } catch (err) {
+    console.log(err.message);
+  }
+  console.log(quote)
 
-      if (
-        typeof body != "object" ||
-        !("quote" in body) ||
-        !("author" in body)
-      ) {
-        console.error(
-          `Failed to extract quote and author from post body: ${bodyString}`
-        );
-        res.status(400).json({
-          succes: false,
-          msg: "Expected body to be a JSON object containing keys quote and author.",
-        });
-        return;
-      }
-
-      if (!body.quote && !body.author) {
-        console.error(`Both quote and author must be provided`);
-        res.status(400).json({
-          success: false,
-          msg: "Both quote and author must be provided",
-        });
-        return;
-      }
-
-      if (!body.quote) {
-        console.error(`Quote must be provided`);
-        res.status(400).json({ success: false, msg: "Please provide a quote" });
-        return;
-      }
-
-      if (!body.author) {
-        console.error(`Author must be provided`);
-        res
-          .status(400)
-          .json({ success: false, msg: "Please provide an author" });
-        return;
-      }
-
-      quotes.push({
-        quote: body.quote,
-        author: body.author,
-      });
-      res.status(201).json({ succes: true, quote: body });
+  if (typeof quote != "object" || !("quote" in quote) || !("author" in quote)) {
+    console.error(
+      `Failed to extract quote and author from post quote: ${quote}`
+    );
+    res.status(400).json({
+      success: false,
+      msg: "Expected quote to be a JSON object containing keys quote and author.",
     });
-  };
+    return;
+  }
+
+  if (!quote.quote && !quote.author) {
+    console.error(`Both quote and author must be provided`);
+    res.status(400).json({
+      success: false,
+      msg: "Both quote and author must be provided",
+    });
+    return;
+  }
+
+  if (!quote.quote) {
+    console.error(`Quote must be provided`);
+    res.status(400).json({ success: false, msg: "Please provide a quote" });
+    return;
+  }
+
+  if (!quote.author) {
+    console.error(`Author must be provided`);
+    res.status(400).json({ success: false, msg: "Please provide an author" });
+    return;
+  }
+
+  quotes.push({
+    quote: quote.quote,
+    author: quote.author,
+  });
+  res.status(201).json({ success: true, quote: quote });
 };
 
 export { createQuote, getQuote };
