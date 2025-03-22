159bdf933bcabc2280016beb9ea1a8d495a1f02c
eediallo <elhhabdouldiallo@gmail.com>
2025-03-22 15:04:09 +0000

    Add deleteQuote route to handle quote deletion

--- backend/routes/quote.js
+++ backend/routes/quote.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createQuote, getAllQuotes, getQuote } from "../controllers/quotes.js";
+import {
+  createQuote,
+  getAllQuotes,
+  getQuote,
+  deleteQuote,
+} from "../controllers/quotes.js";
 
 const quoteRouter = express.Router();
 
 quoteRouter.get("/api/v1/quote", getQuote);
 quoteRouter.get("/api/v1/quotes", getAllQuotes);
+quoteRouter.delete("/api/v1/quote/:id", deleteQuote);
 quoteRouter.post("/", createQuote);
 
 export { quoteRouter };
