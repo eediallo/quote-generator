cd9a60425e61731a279fb371548f61321ad1c2fb
eediallo <elhhabdouldiallo@gmail.com>
2025-03-22 09:06:55 +0000

    Add error exports for centralized error handling

---
+++ backend/errors/index.js
@@ -0,0 +1,10 @@
+import { CustomAPIError } from "./customApiError.js";
+import { BadRequestError } from "./badRequestError.js";
+import { notFoundError } from "./notFoundError.js";
+
+
+export {
+    CustomAPIError,
+    BadRequestError,
+    notFoundError
+}
