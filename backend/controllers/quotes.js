3427239d321d202d11a20081f5336bfd8b144c62
eediallo <elhhabdouldiallo@gmail.com>
2025-03-22 13:24:09 +0000

    Refactor quote error handling and simplify createQuote logic

--- backend/controllers/quotes.js
+++ backend/controllers/quotes.js
@@ -1,7 +1,7 @@
 import { Quote } from "../models/quotes.js";
 import { quotes } from "../db/quotes.js";
 import { asyncWrapper } from "../middleware/async.js";
-import { notFoundError, BadRequestError } from "../errors/index.js";
+import { NotFoundError, BadRequestError } from "../errors/index.js";
 
 function randomQuote(quotes) {
   const index = Math.floor(Math.random() * quotes.length);
@@ -18,7 +18,7 @@ const getAllQuotes = asyncWrapper(async (req, res) => {
 const getQuote = asyncWrapper(async (req, res) => {
   const quotesData = await quotes();
   if (quotesData.length === 0) {
-    throw new notFoundError("No quotes found. Please add a quote");
+    throw new NotFoundError("No quotes found. Please add a quote");
   }
   const quote = randomQuote(quotesData);
   res.status(200).json({ success: true, quote });
@@ -26,32 +26,12 @@ const getQuote = asyncWrapper(async (req, res) => {
 
 // create a quote
 const createQuote = asyncWrapper(async (req, res) => {
-  const quotesData = await quotes();
-  if (quotesData.length === 0) {
-    throw new notFoundError("No quotes found. Please add a quote");
+  const { quote, author } = req.body;
+  if (!quote || !author) {
+    throw new BadRequestError("Please provide quote and author.");
   }
-
-  const quote = await Quote.create(req.body);
-
-  if (
-    typeof quote !== "object" ||
-    !("quote" in quote) ||
-    !("author" in quote)
-  ) {
-    throw new BadRequestError(
-      "Expected quote to be a JSON object containing keys quote and author."
-    );
-  }
-
-  if (!quote.quote || !quote.author) {
-    throw new notFoundError("Please provide a quote and an author");
-  }
-  // add quote to db
-  quotesData.push({
-    quote: quote.quote,
-    author: quote.author,
-  });
-  res.status(201).json({ success: true, quote });
+  const newQuote = await Quote.create(req.body);
+  res.status(201).json({ success: true, newQuote });
 });
 
 export { createQuote, getQuote, getAllQuotes };
