209b61cc241317dc45ba5ae9ee615373be841f86
eediallo <elhhabdouldiallo@gmail.com>
2025-03-22 13:23:11 +0000

    Remove commented-out code from error handler for cleaner implementation

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,7 +1,6 @@
 import { CustomAPIError } from "../errors/customApiError.js";
 export const errorHandler = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
-    // return res.status(err.statusCode).json({ msg: err.message });
     return res
       .status(err.statusCode)
       .json({ success: false, msg: err.message });
