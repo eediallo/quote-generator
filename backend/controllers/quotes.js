e93fe6a3d3cd2e74ff4fc8f8c0c2fcd7675c2f62
eediallo <elhhabdouldiallo@gmail.com>
2025-03-22 09:07:15 +0000

    Refactor quote controller to utilize custom error classes for improved error handling

--- backend/controllers/quotes.js
+++ backend/controllers/quotes.js
@@ -1,6 +1,7 @@
 import { Quote } from "../models/quotes.js";
 import { quotes } from "../db/quotes.js";
 import { asyncWrapper } from "../middleware/async.js";
+import { notFoundError, BadRequestError } from "../errors/index.js";
 
 function randomQuote(quotes) {
   const index = Math.floor(Math.random() * quotes.length);
@@ -17,9 +18,7 @@ const getAllQuotes = asyncWrapper(async (req, res) => {
 const getQuote = asyncWrapper(async (req, res) => {
   const quotesData = await quotes();
   if (quotesData.length === 0) {
-    return res
-      .status(404)
-      .json({ success: false, msg: "No quotes found. Please add a quote" });
+    throw new notFoundError("No quotes found. Please add a quote", 404);
   }
   const quote = randomQuote(quotesData);
   res.status(200).json({ success: true, quote });
@@ -29,46 +28,30 @@ const getQuote = asyncWrapper(async (req, res) => {
 const createQuote = asyncWrapper(async (req, res) => {
   const quotesData = await quotes();
   if (quotesData.length === 0) {
-    return res
-      .status(404)
-      .json({ success: false, msg: "No quotes found. Please add a quote" });
+    throw new notFoundError("No quotes found. Please add a quote", 404);
   }
 
-  // create quote from Quote model
-  let quote = await Quote.create(req.body);
+  let quote;
+  try {
+    quote = await Quote.create(req.body);
+  } catch (error) {
+    throw new BadRequestError("Failed to create quote. Invalid input", 400);
+  }
 
-  if (typeof quote != "object" || !("quote" in quote) || !("author" in quote)) {
-    console.error(
-      `Failed to extract quote and author from post quote: ${quote}`
+  if (
+    typeof quote !== "object" ||
+    !("quote" in quote) ||
+    !("author" in quote)
+  ) {
+    throw new BadRequestError(
+      "Expected quote to be a JSON object containing keys quote and author.",
+      400
     );
-    res.status(400).json({
-      success: false,
-      msg: "Expected quote to be a JSON object containing keys quote and author.",
-    });
-    return;
   }
 
-  if (!quote.quote && !quote.author) {
-    console.error(`Both quote and author must be provided`);
-    res.status(400).json({
-      success: false,
-      msg: "Both quote and author must be provided",
-    });
-    return;
+  if (!quote.quote || !quote.author) {
+    throw new notFoundError("Please provide a quote and an author", 404);
   }
-
-  if (!quote.quote) {
-    console.error(`Quote must be provided`);
-    res.status(400).json({ success: false, msg: "Please provide a quote" });
-    return;
-  }
-
-  if (!quote.author) {
-    console.error(`Author must be provided`);
-    res.status(400).json({ success: false, msg: "Please provide an author" });
-    return;
-  }
-
   // add quote to db
   quotesData.push({
     quote: quote.quote,
