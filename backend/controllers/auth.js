9ab1bf2c5351f931912b2ee546a462ffbfcb711f
eediallo <elhhabdouldiallo@gmail.com>
2025-03-24 05:12:08 +0000

    Add user registration functionality with password hashing

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,13 @@
 import { User } from "../models/user.js";
+import { StatusCodes } from "http-status-codes"
+import bcrypt from "bcryptjs";
 
 const register = async (req, res) => {
-  res.send("Register");
+  const { name, email, password } = req.body;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(req.body.password, salt);
+  const user = await User.create({ name, email, password: passwordHash });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
