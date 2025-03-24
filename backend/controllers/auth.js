31d6781a564c78525cc6f79b05d22880b6db57db
eediallo <elhhabdouldiallo@gmail.com>
2025-03-24 05:38:33 +0000

    Add unauthenticated error handling and refactor auth controller to use async wrapper

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,24 +1,26 @@
 import { BadRequestError } from "../errors/badRequestError.js";
+import { UnauthenticatedError } from "../errors/unauthenticatedError.js";
 import { User } from "../models/user.js";
 import { StatusCodes } from "http-status-codes";
+import { asyncWrapper } from "../middleware/async.js";
 
-const register = async (req, res) => {
+const register = asyncWrapper(async (req, res) => {
   const user = await User.create({ ...req.body });
   const token = await user.createJWT();
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
-};
+});
 
-const login = async (req, res) => {
+const login = asyncWrapper(async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
     throw new BadRequestError("Please provide email and password");
   }
   const user = await User.findOne({ email });
   if (!user) {
-    throw new BadRequestError("Invalid credentials");
+    throw new UnauthenticatedError("Invalid credentials");
   }
   const token = await user.createJWT();
   res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
-};
+});
 
 export { register, login };
