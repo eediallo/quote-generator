6c2c2a7446d0bd42eed92badc9b0449458dea7ed
eediallo <elhhabdouldiallo@gmail.com>
2025-04-05 12:36:03 +0100

    Refactor quote routes to simplify endpoint paths

--- backend/routes/quote.js
+++ backend/routes/quote.js
@@ -9,8 +9,8 @@ import { protect } from "../middleware/auth.js";
 
 const quoteRouter = express.Router();
 
-quoteRouter.get("/api/v1/quote", getQuote);
-quoteRouter.get("/api/v1/quotes", getAllQuotes);
+quoteRouter.get("/quote", getQuote);
+quoteRouter.get("/", getAllQuotes);
 quoteRouter.delete("/api/v1/quote/:id", protect, deleteQuote);
 quoteRouter.post("/", createQuote);
 
