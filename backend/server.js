3033534d41bd993b6e15d9a8f5d84db6dd900252
eediallo <elhhabdouldiallo@gmail.com>
2025-04-05 12:36:16 +0100

    Add Swagger documentation for API and update root route

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,9 @@ import dotenv from "dotenv";
 import { notFound } from "./middleware/notFound.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import { authRouter } from "./routes/user.js";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUiExpress from "swagger-ui-express";
 dotenv.config();
-
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,18 +18,32 @@ app.use(cors());
 // parse json
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send(`
-     <h1>
-        eediallo Quote Server
-    </h1>
-    <p> <a href="/api/v1/quotes">All quotes</a></p>
-    <p><a href="/api/v1/quote">Random quote</a></p>
-    `);
-});
+// Swagger configuration
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "My API",
+      version: "1.0.0",
+      description: "A simple Express API",
+    },
+    servers: [
+      {
+        url: "https://eediallo-qoute-server.hosting.codeyourfuture.io/api/v1/quotes",
+      },
+    ],
+  },
+  apis: ["./collection.yml"], // Path to the API docs
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+if (process.env.NODE_ENV !== "production") {
+  app.use("/", swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerSpec));
+}
 
 // routes
-app.use("/", quoteRouter);
+app.use("/api/v1/quotes", quoteRouter);
 app.use("/api/v1/auth", authRouter);
 
 // page not found handler
