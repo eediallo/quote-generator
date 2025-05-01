bec098f5870be7d37b962386cbb1e2170329d8fd
eediallo <elhhabdouldiallo@gmail.com>
2025-05-01 10:15:47 +0100

    Update test for getRandomQuote to validate input type and add additional case

--- backend/tests/controllers/quotes.test.js
+++ backend/tests/controllers/quotes.test.js
@@ -3,9 +3,16 @@ import { it, expect, describe } from "vitest";
 import { getRandomQuote } from "../../controllers/quotes.js";
 
 describe("getRandomQuote()", () => {
-  it("Should throw an error if parameter is not of type array", () => {
+
+  it("Should throw an error if parameter is not an array", () => {
     const input = "invalid";
+    const input1 = {}
+
     const resultFn = () => getRandomQuote(input);
+     const resultFn1 = () => getRandomQuote(input1);
+
     expect(resultFn).toThrowError("Input must be an array");
+    expect(resultFn1).toThrowError("Input must be an array");
   });
+  
 });
